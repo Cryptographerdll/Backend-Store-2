b6e1e27e174b1fc9fc648b97d5bfc2ab87d65223
Cryptographer <85350190+CryptographyHacker@users.noreply.github.com>
2022-01-15 15:23:31 +0100

    Add files via upload

---
+++ AuthorRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models.Repositories
+{
+    public class AuthorRepository : IbookStoreRepositorie <Author>
+    {
+        IList <Author> authors;
+
+        public AuthorRepository()
+        {
+            authors = new List<Author>()
+            {
+                new Author
+                {
+                    Id = 1,
+                    FullName = "Mark Selby"
+                },
+
+                new Author
+                {
+                    Id = 2,
+                    FullName = "Neil Robertson"
+                },
+
+                new Author
+                {
+                    Id = 3,
+                    FullName = "The Man"
+                }
+            };
+        }
+
+        public void Add(Author entity)
+        {
+            authors.Add(entity);
+        }
+
+        public void Delete(int Id)
+        {
+            var author = Find(Id);
+
+            authors.Remove(author);
+        }
+
+        public Author Find(int Id)
+        {
+            var author = Find(Id);
+
+            return author;
+        }
+
+        public IList<Author> List()
+        {
+            return authors;
+        }
+
+        public void Update(int Id, Author newAuthor)
+        {
+            var author = authors.SingleOrDefault(a => a.Id == Id);
+
+            author.Id = newAuthor.Id;
+            author.FullName = newAuthor.FullName;
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,53 @@
+using BookStore.Models;
+using BookStore.Models.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddMvc();
+            services.AddSingleton <IbookStoreRepositorie <Author>, AuthorRepository>();
+            services.AddSingleton<IbookStoreRepositorie <Book>, BookRepository>();
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+        }
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+       
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseMvcWithDefaultRoute();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello world");
+            });
+        }
+    }
+}
